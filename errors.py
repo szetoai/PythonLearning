4aab79627941ed6584c854125bbf7fe275b97c5a
AidanS99910 <szeto.aidan@doversherborn.org>
2024-07-16 21:11:53 -0400

    errors

---
+++ errors.py
@@ -0,0 +1,34 @@
+""""
+# syntax
+print "hello"
+
+# name
+print(var)
+
+# index
+x = []
+print(x[2])
+
+# modulenotfound
+import dfkasjflasj
+
+# attribute
+import math
+math.fdjlkajfl
+
+# key
+mydict = {}
+print(mydict["AAA"])
+
+# type
+print(4 + "hi")
+
+# import
+from math import fajsljf
+
+# value
+int("hi")
+
+# zerodivision
+5/0
+"""
