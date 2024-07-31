71efafd2e3966bef451edce35ffdff7454656f5c
ASzeto99910 <83287704+AidanS99910@users.noreply.github.com>
2024-07-31 12:28:13 -0400

    D20work

---
+++ D20Packages.py
@@ -0,0 +1,4 @@
+from D20REF_Packages import D20REF_mathing as mathing, D20REF_greet as greet
+
+print(greet.greet_person("A", "Szeto"))
+print(mathing.add_numbers(5, 1, 3, 4, 9428))
--- D20Pip.py
+++ D20Pip.py
@@ -2,15 +2,30 @@
 installing: pip install numpy, pip install pandas
 deleting: pip uninstall packagename
 see installed packages: pip list
-show package info: pip show packagename
+show package info: pip show packagename ("show --verbose" for more details)
+show packages used: pip freeze
 '''
 
 import numpy as np, pandas as pd, webbrowser as wb
 
+"""
+# numpy
 print(np.version.version)
 lst = [1, 2, 3, 4, 5]
 np_arr = np.array(lst)
 print(np_arr)
 print(len(np_arr))
 print(np_arr * 2 + 3)
+# webbrowser
 wb.open_new_tab("youtube.com")
+
+
+# requests
+import requests
+response = requests.get('https://httpbin.org/basic-auth/user/pass', auth=('user', 'pass'))  # opening a network and fetching a data
+print(response) # response object
+print(response.status_code)  # status code, success:200 
+print(response.json())  # response as json
+print(response.text) # response as text
+print(response.headers) # response's headers
+"""
