5f70b43d37714f8374a85be71ff7acac4b16a568
AidanS99910 <szeto.aidan@doversherborn.org>
2024-08-18 18:19:03 -0400

    cool

--- Projects.py
+++ Projects.py
@@ -1,6 +1,18 @@
 class Solution:
-    def isValid(self, s):
-        for x in s:
-            
+    def carFleet(self, target: int, position, speed) -> int:
+        cars = [a for a in zip(position,speed)]
+        cars.sort()
+        stack = []
+        def time(pos,mph):
+            return (target-pos)/mph
+        for x in range(len(cars)-1,-1,-1):
+            if x == len(cars)-1:
+                stack.append(time(cars[x][0],cars[x][1]))
+            elif stack[-1] < time(cars[x][0],cars[x][1]):
+                stack.append(time(cars[x][0],cars[x][1]))
+        return len(stack)
+
+
+
 L = Solution()
-L.isValid()
+print(L.carFleet(target = 10, position = [4,1,0,7], speed = [2,2,1,1]))
--- test.py
+++ test.py
@@ -1,2 +1,4 @@
-p = "hi"
-print(p[1])
+a = [1,2,3]
+b = [4,5,6]
+c = [x for x in zip(a,b)]
+print(c.sort)
