fc2d7f9ab554397150695653f7f60a0b9938c34f
AidanS99910 <szeto.aidan@doversherborn.org>
2024-08-25 09:13:55 -0400

    d26

--- Projects.py
+++ Projects.py
@@ -1,18 +1,28 @@
 class Solution:
-    def carFleet(self, target: int, position, speed) -> int:
-        cars = [a for a in zip(position,speed)]
-        cars.sort()
-        stack = []
-        def time(pos,mph):
-            return (target-pos)/mph
-        for x in range(len(cars)-1,-1,-1):
-            if x == len(cars)-1:
-                stack.append(time(cars[x][0],cars[x][1]))
-            elif stack[-1] < time(cars[x][0],cars[x][1]):
-                stack.append(time(cars[x][0],cars[x][1]))
-        return len(stack)
-
+    def searchMatrix(self, matrix, target):
+        # coords of min and max [y,x]
+        min = [0,0]
+        max = [len(matrix)-1,len(matrix[0])-1]
+        if max == min or (min[0] == max[0] and min[1]+1 == max[1]) or :
+            if matrix[max[0]][max[1]] == target or matrix[min[0]][min[1]] == target:
+                return True
+            else:
+                return False
+        # while min and max arent essentially 1 element away horizontally
+        while min[0] != max[0] or min[1] + 1 != max[1]:
+            # mid coords [y,x]
+            minpos = min[0] * len(matrix[0]) + min[1]
+            maxpos = max[0] * len(matrix[0]) + max[1]
+            minpos = (minpos + maxpos)/2
+            mid = [int(minpos/len(matrix[0])),int(minpos%len(matrix[0]))]
+            if matrix[mid[0]][mid[1]] == target:
+                return True
+            elif matrix[mid[0]][mid[1]] > target:
+                max = mid
+            elif matrix[mid[0]][mid[1]] < target:
+                min = mid
+        return False
 
 
 L = Solution()
-print(L.carFleet(target = 10, position = [4,1,0,7], speed = [2,2,1,1]))
+print(L.searchMatrix(matrix=[[1,1]],target=1))
--- test.py
+++ test.py
@@ -1,4 +1,1 @@
-a = [1,2,3]
-b = [4,5,6]
-c = [x for x in zip(a,b)]
-print(c.sort)
+print(5.5 % 4)
